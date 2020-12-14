71577a8b70a0c3587ab0f2ee1b324e45270a6ea8
Aleksei Gubin <gubin911@yandex.ru>
2020-12-14 22:08:11 +0300

    init

---
+++ client/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"grpc-chat/protos"
+	"os"
+
+	"log"
+	"sync"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var client protos.ChatClient
+var wait *sync.WaitGroup
+
+func init() {
+	wait = &sync.WaitGroup{}
+}
+
+func connect(user *protos.User) error {
+	var streamerror error
+
+	stream, err := client.Connect(context.Background(), &protos.User{
+		Name:   user.Name,
+		Active: true,
+	})
+
+	if err != nil {
+		return fmt.Errorf("connection failed: %v", err)
+	}
+
+	wait.Add(1)
+	go func(str protos.Chat_ConnectClient) {
+		defer wait.Done()
+
+		for {
+			msg, err := str.Recv()
+			if err != nil {
+				streamerror = fmt.Errorf("Error reading message: %v", err)
+				break
+			}
+
+			fmt.Printf("%v : %s\n", msg.User.Name, msg.Message)
+		}
+	}(stream)
+
+	return streamerror
+}
+
+func main() {
+	done := make(chan int)
+
+	fmt.Print("username: ")
+	var name string
+	fmt.Scan(&name)
+
+	user := &protos.User{
+		Name: name,
+	}
+
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Couldnt connect to service: %v", err)
+	}
+
+	client = protos.NewChatClient(conn)
+
+	connect(user)
+
+	wait.Add(1)
+	go func() {
+		defer wait.Done()
+
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			msg := &protos.Message{
+				User:    user,
+				Message: scanner.Text(),
+			}
+
+			_, err := client.SendMessage(context.Background(), msg)
+			if err != nil {
+				fmt.Printf("Error Sending Message: %v", err)
+				break
+			}
+		}
+
+	}()
+
+	go func() {
+		wait.Wait()
+		close(done)
+	}()
+
+	<-done
+}
---
+++ server/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"grpc-chat/protos"
+	"log"
+	"net"
+	"os"
+	"sync"
+
+	"google.golang.org/grpc"
+	glog "google.golang.org/grpc/grpclog"
+)
+
+var grpcLog glog.LoggerV2
+
+func init() {
+	grpcLog = glog.NewLoggerV2(os.Stdout, os.Stdout, os.Stdout)
+}
+
+// Connection ...
+type Connection struct {
+	stream protos.Chat_ConnectServer
+	name   string
+	active bool
+	err    chan error
+}
+
+// Server ...
+type Server struct {
+	Connection []*Connection
+}
+
+// Connect ...
+func (s *Server) Connect(user *protos.User, stream protos.Chat_ConnectServer) error {
+	conn := &Connection{
+		stream: stream,
+		name:   user.Name,
+		active: true,
+		err:    make(chan error),
+	}
+	s.Connection = append(s.Connection, conn)
+	return <-conn.err
+}
+
+// SendMessage ...
+func (s *Server) SendMessage(ctx context.Context, msg *protos.Message) (*protos.Close, error) {
+	wait := sync.WaitGroup{}
+	done := make(chan int)
+
+	for _, conn := range s.Connection {
+		log.Println(conn.name)
+		wait.Add(1)
+
+		go func(msg *protos.Message, conn *Connection) {
+			defer wait.Done()
+
+			if conn.active {
+				err := conn.stream.Send(msg)
+				grpcLog.Infof("Sending message %v to user %v", msg.User.Name, conn.name)
+
+				if err != nil {
+					grpcLog.Errorf("Errors with stream %v: %v", conn.stream, err)
+					conn.active = false
+					conn.err <- err
+				}
+			}
+		}(msg, conn)
+	}
+
+	go func() {
+		wait.Wait()
+		close(done)
+	}()
+
+	<-done
+	return &protos.Close{}, nil
+}
+
+func main() {
+	var connections []*Connection
+
+	server := &Server{connections}
+
+	grpcServer := grpc.NewServer()
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("error creating the server %v", err)
+	}
+
+	grpcLog.Info("Starting server at port :8080")
+
+	protos.RegisterChatServer(grpcServer, server)
+	grpcServer.Serve(listener)
+}
